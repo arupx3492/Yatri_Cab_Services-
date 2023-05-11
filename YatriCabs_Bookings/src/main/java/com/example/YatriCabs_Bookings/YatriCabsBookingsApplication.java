a96bf86ce6fd24501e7c8115bf6ceadd3ea92574
Arup Mahato <arup71062@gmail.com>
2023-05-11 09:26:03 +0530

    Basic Structured

---
+++ YatriCabs_Bookings/src/main/java/com/example/YatriCabs_Bookings/YatriCabsBookingsApplication.java
@@ -0,0 +1,13 @@
+package com.example.YatriCabs_Bookings;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YatriCabsBookingsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YatriCabsBookingsApplication.class, args);
+	}
+
+}
